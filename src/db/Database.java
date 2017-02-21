950832a2faa98a8d04319826c7bb33deb942fe8b
sklee <hadesleeq@gmail.com>
2017-02-21 16:52:35 +0900

    사용자 데이터 저장 및 sendRedirect 사용

---
+++ src/db/Database.java
@@ -0,0 +1,16 @@
+package db;
+
+import java.util.Map;
+
+/**
+ * Project: EmbeddedTomcat
+ * FileName: Database
+ * Date: 2017-02-21
+ * Time: 오후 4:50
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Database {
+    private static Map<String, >
+}
---
+++ src/user/User.java
@@ -0,0 +1,17 @@
+package user;
+
+/**
+ * Project: EmbeddedTomcat
+ * FileName: User
+ * Date: 2017-02-21
+ * Time: 오후 4:51
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class User {
+    private String userId;
+    private String password;
+    private String name;
+    private String email;
+}
