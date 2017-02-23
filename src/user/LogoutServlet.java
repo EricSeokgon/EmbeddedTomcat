935930f0c0a7a61932d091f35cf2c483b6bd2f6b
sklee <hadesleeq@gmail.com>
2017-02-23 10:57:13 +0900

    logout Servlet 추가

---
+++ src/user/LogoutServlet.java
@@ -0,0 +1,26 @@
+package user;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * Project: EmbeddedTomcat
+ * FileName: LogoutServlet
+ * Date: 2017-02-23
+ * Time: 오전 10:54
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class LogoutServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        session.removeAttribute("userId");
+        response.sendRedirect("/");
+    }
+}
